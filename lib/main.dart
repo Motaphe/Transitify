fa53c6cd36c241a509b86bc015c6fb3541e6937d
daredevilx616 <abdullahnasirpti@gmail.com>
2023-02-05 07:37:42 -0500

    Update3

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-
+import 'package:idea11/locations.dart';
 import 'main2.dart';
 
-const double lat = 33.753746;
+const double lati = 33.753746;
 const double long = -84.386330;
 const double zoom = 14.4746;
 
@@ -28,6 +29,9 @@ class MyStatefulWidget extends StatefulWidget {
 }
 
 class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  TextEditingController _searchController = TextEditingController();
+  TextEditingController _searchController2 = TextEditingController();
+
   int _selectedIndex = 0;
   static const TextStyle optionStyle =
       TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
@@ -43,11 +47,50 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
   ];
 
   Completer<GoogleMapController> _controller = Completer();
+  late String _destination;
+  Set<Marker> _markers = Set<Marker>();
+  Set<Polyline> _polylines = Set<Polyline>();
+
+  int _polylineIdCounter = 1;
+
+  @override
+  void initState() {
+    super.initState();
+    _setMarker(LatLng(lati, long));
+  }
+
+  void _setMarker(LatLng point) {
+    setState(() {
+      _markers.add(
+        Marker(
+          markerId: MarkerId('Marker'),
+          position: point,
+        ),
+      );
+    });
+  }
 
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
     });
+  }
+
+  void _setPolyline(List<PointLatLng> points) {
+    final String polylineIdVal = 'polyline_$_polylineIdCounter';
+    _polylineIdCounter++;
+    _polylines.add(
+      Polyline(
+        polylineId: PolylineId(polylineIdVal),
+        width: 2,
+        color: Colors.blue,
+        points: points
+            .map(
+              (points) => LatLng(points.latitude, points.longitude),
+            )
+            .toList(),
+      ),
+    );
   }
 
   @override
@@ -58,6 +101,52 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
       body = Column(
         children: <Widget>[
           Container(
+            padding: EdgeInsets.all(10.0),
+            child: TextField(
+              controller: _searchController,
+              textCapitalization: TextCapitalization.words,
+              decoration: InputDecoration(
+                hintText: 'Origin',
+              ),
+              onChanged: (value) {
+                setState(() {
+                  _destination = value;
+                });
+              },
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(10.0),
+            child: TextField(
+              controller: _searchController2,
+              textCapitalization: TextCapitalization.words,
+              decoration: InputDecoration(
+                hintText: 'Destination',
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.search),
+                  onPressed: () async {
+                    var directions = await LocationService().getDirections(
+                        _searchController.text, _searchController2.text);
+                    //var place = await LocationService()
+                    //  .getPlace(_searchController2.text);
+                    _goToPlace(
+                      directions['start_location']['lat'],
+                      directions['start_location']['lng'],
+                      directions['bounds_ne'],
+                      directions['bounds_sw'],
+                    );
+                    _setPolyline(directions['polyline_decoded']);
+                  },
+                ),
+              ),
+              onChanged: (value) {
+                setState(() {
+                  _destination = value;
+                });
+              },
+            ),
+          ),
+          Container(
             height: MediaQuery.of(context).size.height / 2,
             decoration: BoxDecoration(
               border: Border.all(
@@ -66,8 +155,10 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
               ),
             ),
             child: GoogleMap(
+              markers: _markers,
+              polylines: _polylines,
               initialCameraPosition: CameraPosition(
-                target: LatLng(lat, long),
+                target: LatLng(lati, long),
                 zoom: zoom,
               ),
               onMapCreated: (GoogleMapController controller) {
@@ -104,5 +195,30 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
         onTap: _onItemTapped,
       ),
     );
+  }
+
+  Future<void> _goToPlace(
+    double lat,
+    double lng,
+    Map<String, dynamic> boundsNe,
+    Map<String, dynamic> boundsSw,
+  ) async {
+    //final double lat = place['geometry']['location']['lat'];
+    //final double lng = place['geometry']['location']['lng'];
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(
+      CameraUpdate.newCameraPosition(
+        CameraPosition(target: LatLng(lat, lng), zoom: 12),
+      ),
+    );
+    controller.animateCamera(
+      CameraUpdate.newLatLngBounds(
+          LatLngBounds(
+            southwest: LatLng(boundsSw['lat'], boundsSw['lng']),
+            northeast: LatLng(boundsNe['lat'], boundsNe['lng']),
+          ),
+          25),
+    );
+    _setMarker(LatLng(lat, lng));
   }
 }
